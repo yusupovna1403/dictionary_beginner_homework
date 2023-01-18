fda7978bf3aedf34f2f40990183a6ceb1c36e434
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-19 00:13:27 +0500

    dictionary_02

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -8,4 +8,26 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    return 
+    sum = 0
+    for item in data:
+        if item['age'] == age:
+            sum+=1
+
+    return sum
+
+data = [
+  {
+    'name': 'John',
+    'age': 27
+  },
+  {
+    'name':'Mary', 
+    'age': 42
+  },
+  {
+    'name':'Ann',
+    'age': 27
+  }
+  ]
+age = 27
+print(count_users_with_age(data,age)) 
