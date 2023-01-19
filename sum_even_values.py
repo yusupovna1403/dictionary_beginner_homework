ba3ff76d04efc1bab7afd08592f82ebd45e32330
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-19 19:42:18 +0500

    dictionary_15

--- sum_even_values.py
+++ sum_even_values.py
@@ -6,4 +6,20 @@ def sum_even_values(data: dict) -> int:
     Returns:
         int: The sum of all even values in the dictionary
     '''
-    return 
+    sum = 0
+    for i in data.values():
+        if (type(i) == int or type(i) == float) and i % 2 == 0:
+            sum+=i
+    return sum
+    
+
+        
+data = {
+    1: 22, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }
+print(sum_even_values(data))
