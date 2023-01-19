c1d3401baf953a8001376d2bd668a7569c034161
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-19 19:12:33 +0500

    dictionary_12

--- get_user_names.py
+++ get_user_names.py
@@ -8,4 +8,37 @@ def get_user_names(data:list, country:str) -> list:
     Returns:
         list: A list of users with the given country
     """
-    return 
+    lst = []
+    for dict in data:
+        if dict['country'] == country:
+            lst.append(dict['name'])
+    return lst 
+
+data = [
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Henry', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Sam', 
+    'country': 'MEX'
+  },
+  {
+    'name': 'Kevin', 
+    'country': 'RUS'
+  },
+  {
+    'name': 'Dustin', 
+    'country': 'GER'
+  }
+]
+country = 'UK'
+print(get_user_names(data,country))
