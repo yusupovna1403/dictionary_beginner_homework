f868fa91161339015ca97aeea19118f2613df16e
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-19 16:46:40 +0500

    dictionary_04

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,17 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    sum = 0
+    for i in data.values():
+        sum+=len(i)
+    return sum
+    
+
+
+data = {
+    1 : "Khiva", 
+    2 : "Namangan", 
+    3 : "Samarkand", 
+    4 : "Tashkent"
+  }
+print(find_length_of_values(data))
